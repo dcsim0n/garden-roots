e8da5ed0c1e7c651a8247eec9066c3f5a9cace38
Dana Simmons <dana@simmons-audio.com>
2019-03-26 10:17:18 -0400

    we can make suggestions for plants and sort them by soil compatibility

--- garden-api/app/controllers/suggest_controller.rb
+++ garden-api/app/controllers/suggest_controller.rb
@@ -2,6 +2,11 @@ class SuggestController < ApplicationController
     def show
         @bed = GardenBed.find(params[:id])
         
-        render json: @bed, status: :ok
+        #This is pretty bare bones
+        #It would be nice to return results sorted by compatibility, sort by soil type?
+
+        @suggest_plants = Plant.where(sun: @bed.sun).sort_by{|p| p.soil_delta(@bed.soil)}
+
+        render json: @suggest_plants, status: :ok
     end
 end
--- garden-api/app/models/plant.rb
+++ garden-api/app/models/plant.rb
@@ -1,4 +1,9 @@
 class Plant < ApplicationRecord
     has_many :bed_plants
     has_many :garden_beds, through: :bed_plants
+
+    def soil_delta(soil_int)
+        #calculate difference as absolute value
+        (self.soil - soil_int).abs
+    end
 end
