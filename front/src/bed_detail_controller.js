ba6d66465ad4affc5cd877c1a2916a24b7c70715
Chanda Grayson <chandagrayson@gmail.com>
2019-03-27 11:57:48 -0400

    bed detail controller start with pseudocode

---
+++ front/src/bed_detail_controller.js
@@ -0,0 +1,39 @@
+const bedDetail = {
+
+}
+
+function fetchBedDetails() {
+    //fetch on the click function from the bed controller
+    fetch(`http://localhost:3000/garden_beds/${this.id}`)
+    .then(res => res.json())
+    //load details of selected bed
+}
+
+//show larger representation of selected bed
+        //paper.js function of secondLayer
+
+//show side menu of suggested plants by picture and name
+
+//add mouse event(hover over picture?) to show plant details(color?, height?)
+
+//add mouse handler(drag) function to drag and drop plant from side menu into the bed.. representation smaller picture, or just color?
+    function onMouseDrag(event) {
+        const canvas = document.getElementById("paper")
+        const detailPath = new.paper.Path()
+        detailPath.add(event.point)
+        //vector boundary? for circle path of bed (call BedController?)
+    }
+
+//POST will autosave to database
+//(x) button to close and return to all beds view
+
+layers = []
+
+let layer = new Layer()
+     layer.name = firstLayer
+        firstLayer = document.layers[0] //parent
+
+let layer = new Layer()
+    layer.name = secondLayer
+        secondLayer = document.layers[1] //child
+
---
+++ front/src/garden_bed_detail_view.js
@@ -0,0 +1,9 @@
+class GardenBedDetailView {
+
+    constructor(bed, position) {
+        this.bed = bed
+        this.position = position 
+        this.detail = paper.new
+    }
+    
+}
