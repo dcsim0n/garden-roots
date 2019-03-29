31afdfc480907594a84c571887f3937a757a0828
Dana Simmons <dana@simmons-audio.com>
2019-03-29 12:59:19 -0400

    plant suggestions apear content is scrollable

--- front/src/suggested_plants.js
+++ front/src/suggested_plants.js
@@ -3,17 +3,23 @@ const SuggestedPlants = {
     showSuggests: function (event) {
         const bedId = event.target.dataset.id
         const suggestList = document.getElementById("suggestions")
-        suggestList.innerHTML= ""
+        //suggestList.innerHTML= ""
         fetchAll(`${SUGGEST_URL}/${bedId}`,(data)=>{
             data.forEach((plant)=>{
-                this.renderPlant(plant)
+                SuggestedPlants.renderPlant(plant)
             })
         })
     },
 
     renderPlant: function(plantObj){
-        const li = document.createElement("li")
-        li.innerHTML = plant.name
-        suggestList.appendChild(li)
-    }
+        const suggestions = document.getElementById("suggestions")
+        
+        const card = suggestions.lastElementChild.cloneNode(true)
+        card.querySelector("h4").innerHTML = plantObj.name
+        card.querySelector("img").src = plantObj.img_1_url
+        card.hidden = false
+        suggestions.appendChild(card)
+        
+    },
+
 }
